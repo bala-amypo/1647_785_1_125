5660a00c9515481210e0d22e61f96ce354e61496
bala-amypo <bala@amypo.in>
2025-12-26 11:38:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/RecipeIngredientRepository.java
+++ src/main/java/com/example/demo/repository/RecipeIngredientRepository.java
@@ -2,3 +2,7 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
+    List<RecipeIngredient> findByMenuItemId(Long id);
+    Double getTotalQuantityByIngredientId(Long id);
+}
