9f036cdbb5eafd70dbaf0e496814ddcc874d5e1a
bala-amypo <bala@amypo.in>
2025-12-27 03:59:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/MenuItemController.java
+++ src/main/java/com/example/demo/controller/MenuItemController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/menu-items")
-public  MenuItemController {
+public class MenuItemController {
 
     private final MenuItemService menuItemService;
 
